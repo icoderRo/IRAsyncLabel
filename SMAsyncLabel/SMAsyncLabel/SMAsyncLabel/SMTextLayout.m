1136bf74c6e0e26773c2869f4381704d6d1e9d02
simon <simon@simondeMacBook-Pro.local>
2016-11-29 15:59:13 +0800

    init calculate bounding Size
    1. init calculate bounding Size
    2.will store property

--- SMAsyncLabel/SMAsyncLabel/SMAsyncLabel/SMTextLayout.m
+++ SMAsyncLabel/SMAsyncLabel/SMAsyncLabel/SMTextLayout.m
@@ -25,44 +25,91 @@
     NSMutableAttributedString *attText = text.mutableCopy;
     CTFramesetterRef frameSetter = CTFramesetterCreateWithAttributedString((CFTypeRef)attText);
     CFRange range = CFRangeMake(0, (CFIndex)attText.length);
-    CGPathRef path = container.path.CGPath;
+    CGPathRef containerPath = container.path.CGPath;
+    CGRect boundingBox = CGPathGetBoundingBox(containerPath);
     
-//    CTFramesetterCreateFrame(<#CTFramesetterRef  _Nonnull framesetter#>, <#CFRange stringRange#>, <#CGPathRef  _Nonnull path#>, <#CFDictionaryRef  _Nullable frameAttributes#>)
+    // calculate  bounding size
+    CGSize constraints = CGSizeMake(boundingBox.size.width, MAXFLOAT);
+    CGSize suggestSize = CTFramesetterSuggestFrameSizeWithConstraints(frameSetter, range, NULL, constraints, NULL);
+    CGSize size = CGSizeMake(ceil(suggestSize.width), ceil(suggestSize.height));
     
-    
-    
+    // addRect
+    CGMutablePathRef path = CGPathCreateMutable();
+    CGRect rect = {boundingBox.origin,{boundingBox.size.width, size.height}};
+    CGPathAddRect(path, NULL, rect);
+    CTFrameRef frame = CTFramesetterCreateFrame(frameSetter, range, path, NULL);
     
     
     // CTLineRef
     
+    int rowIndex = -1;
+    int rowCount = 0;
+    int curIndex = 0;
+    
+    CGRect lastRect = CGRectMake(0.0f, -CGFLOAT_MAX, 0.0f, 0.0f);
+    CGPoint lastPos = CGPointMake(0.0f, -CGFLOAT_MAX);
+    CGRect textBoundRect = CGRectZero;
+    
     NSMutableArray *linesArray = [NSMutableArray array];
     
+    CFArrayRef ctLines = CTFrameGetLines(frame);
+    CFIndex lineCount = CFArrayGetCount(ctLines);
+    CGPoint *lineOrigins = NULL;
+    if (lineCount > 0) {
+        lineOrigins = malloc(lineCount *sizeof(CGPoint));
+        CTFrameGetLineOrigins(frame, CFRangeMake(0, lineCount), lineOrigins);
+    }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    // CTRunRef
-    
-    
-    
-    
-    
-    
-    
-    
+    for (int i = 0; i < lineCount; i++) {
+        
+        CTLineRef ctLine = CFArrayGetValueAtIndex(ctLines, i);
+        CFArrayRef runs = CTLineGetGlyphRuns(ctLine);
+        CFIndex run = CFArrayGetCount(runs);
+        if (!runs || run == 0) continue;
+        
+        CGPoint lineOrigin = lineOrigins[i];
+        CGPoint position = CGPointMake(rect.origin.x + lineOrigin.x, rect.size.height + rect.origin.y - lineOrigin.y);
+        
+        SMTextLine *line = [SMTextLine sm_textLineWithCTLine:ctLine lineOrigin:position];
+        CGRect lineRect = line.frame;
+        
+        BOOL newRow = YES;
+        if (position.x != lastPos.x) {
+            if (lineRect.size.height > lastRect.size.height) {
+                if (lineRect.origin.y < lastPos.y && lastPos.y < lineRect.origin.y + lineRect.size.height) {
+                    newRow = NO;
+                }
+            } else {
+                if (lastRect.origin.y < position.y && position.y < lastRect.origin.y + lastRect.size.height) {
+                    newRow = NO;
+                }
+            }
+        }
+        if (newRow){
+            rowIndex ++;
+        }
+        
+        lastRect = lineRect;
+        lastPos = position;
+        line.index = curIndex;
+        line.row = rowIndex;
+        [linesArray addObject:line];
+        
+        rowCount = rowIndex + 1;
+        curIndex += 1;
+        if (i == 0) {
+            textBoundRect = lineRect;
+        } else {
+            textBoundRect = CGRectUnion(textBoundRect, lineRect);
+        }
+    }
+
     // Setter
     SMTextLayout *layout = [[SMTextLayout alloc] init];
     layout->_needDrawText = YES;
     layout->_container = container;
     layout->_text = text.mutableCopy;
+    // TODO: setting....
     return layout;
 }
 
